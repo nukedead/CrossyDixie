2faea2bc5e4f7c8053e705d28b89e6aacbafbee8
aquileslm <aquileslm@users.noreply.github.com>
2017-11-29 18:40:20 -0500

    Controles y Creditos creados.

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
 
-    float a = 10f;
+    float a = 20f;
 
     void Start()
     {
@@ -15,19 +15,32 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        Rotate();
         Move();
     }
-    
+    void Rotate()
+    {
+        if (Input.GetKey(KeyCode.Q))
+            transform.Rotate(new Vector3(0f, 0f, -90) * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.E))
+            transform.Rotate(new Vector3(0f, 0f, 90) * Time.deltaTime);
+
+    }
     void Move()
     {
         if (Input.GetKey(KeyCode.W))
-            transform.Translate(Vector3.forward * a * Time.deltaTime);
-        else if (Input.GetKey(KeyCode.S))
-            transform.Translate(Vector3.back * a * Time.deltaTime);
-        else if (Input.GetKey(KeyCode.D))
-            transform.Translate(Vector3.right * a * Time.deltaTime);
-        else if (Input.GetKey(KeyCode.A))
             transform.Translate(Vector3.left * a * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.right * a * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.forward * a * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.back * a * Time.deltaTime);
+        
     }
+
+        
+    
+        
+    
 }
