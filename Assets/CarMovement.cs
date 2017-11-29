3a1df202573034e8c1e8664012d72ee0487ae2a5
pisicato7 <pisicato7@gmail.com>
2017-11-29 14:55:57 -0500

    a
    a

--- Assets/CarMovement.cs
+++ Assets/CarMovement.cs
@@ -2,17 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarMovement : MonoBehaviour {
-	
+public class CarMovement : MonoBehaviour
+{
 
-		public Rigidbody rb;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		rb.AddForce (80,0,0);
-	}
+
+    public GameObject car;
+    public Rigidbody rb;
+    public Vector3 pos = new Vector3();
+    public float spawnTime = 2f;
+    public Transform[] spawnPoints;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+        /* rb = GetComponent<Rigidbody>();
+         if (rb.transform.position.x >140)
+         {
+             Destroy(rb);
+            // Instantiate(rb);
+         }
+        else
+         {
+             Update();
+         }*/
+    }
+
+
+    void Spawn()
+    {
+        if (car.transform.position.x > 140)
+        {
+            Destroy(car);
+
+        }
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length); //indice de creación
+        Instantiate(car, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //  Instantiate(rb);
+        rb.AddForce(100 * Time.deltaTime, 0, 0);
+    }
 }
+
