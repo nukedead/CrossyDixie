daf59a0bb09e6d3a7a1e89a6739ea6187cb260c1
pisicato7 <pisicato7@gmail.com>
2017-11-29 12:14:05 -0500

    Generación de carros por periodo de timepo
    No aparecen en un punto en específico

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -5,28 +5,45 @@ using UnityEngine;
 public class CarMovement : MonoBehaviour {
 
 
-  //  public GameObject car;
-    public Rigidbody rb;
-    public Vector3 position = new Vector3();
-
+    public GameObject car;
+     public Rigidbody rb;
+    public Vector3 pos = new Vector3();
+    public float spawnTime = 2f;
+    public Transform[] spawnPoints;
 
 	// Use this for initialization
 	void Start () {
-     
-            Instantiate(rb);
-      
+
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+       /* rb = GetComponent<Rigidbody>();
         if (rb.transform.position.x >140)
         {
             Destroy(rb);
+           // Instantiate(rb);
         }
-        else
+       else
         {
             Update();
-        }
+        }*/
     }
+
+
+    void Spawn ()
+    {
+        if (car.transform.position.x > 140)
+        {
+            Destroy(car);
+
+        }
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length); //indice de creación
+        Instantiate(car, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+
+    }
+
 	
 	// Update is called once per frame
 	void Update () {
-		rb.AddForce (20 * Time.deltaTime,0,0 );
+        //  Instantiate(rb);
+        rb.AddForce (100 * Time.deltaTime,0,0 );
 	}
 }
