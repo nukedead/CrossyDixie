6af8eca5b3260e3ac073a94f0e617c3e7e4c557f
pisicato7 <pisicato7@gmail.com>
2017-11-29 00:02:17 -0500

    Modificando scripts de carros
    carros

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CarMovement : MonoBehaviour {
-	
 
-		public Rigidbody rb;
+
+    public GameObject car;
+    public Vector3 Pos;
+
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+     
+            Instantiate(car);
+           
+       
+    }
 	
 	// Update is called once per frame
 	void Update () {
